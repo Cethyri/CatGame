cfc44c23902bb9e8f2a8ede0e891f5875e2a4607
Christopher <crobinson@student.neumont.edu>
2016-11-23 15:41:20 -0700

    Begin adding attributes and methods to Kitten

--- src/neumont/thanksgiving/gameJam/Kitten.java
+++ src/neumont/thanksgiving/gameJam/Kitten.java
@@ -1,12 +1,56 @@
-/**
- * 
- */
 package neumont.thanksgiving.gameJam;
 
-/**
- * @author Christopher
- *
- */
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
 public class Kitten {
 
+	private static final int PLAYER_WIDTH = 24 * Finals.PIXEL_RATIO, PLAYER_HEIGHT = 24 * Finals.PIXEL_RATIO;
+
+	private static final String PATH = "Images/", END = ".png";
+
+	private int posx, posy, dx, dy;
+
+	private int frame;
+	private Image sprite;
+
+	public Kitten(int startX, int startY) {
+		
+		posx = startX;
+		posy = startY - PLAYER_HEIGHT;
+
+		dx = 0;
+		dy = 0;
+
+		frame = 0;
+	}
+
+	public void move() {
+		posx += dx;
+		posy += dy;
+		
+		animate();
+	}
+
+	private void animate() {
+		if (frame > 3) {
+			frame = 0;
+		}
+		sprite = new ImageIcon(PATH + "Idle Sprite " + frame + END).getImage();
+		frame++;
+	}
+	
+	public void draw(Graphics g) {
+		
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g2d.drawImage(sprite, posx, posy, null);
+	}
+
+	public Image getSprite() {
+		return sprite;
+	}
 }
