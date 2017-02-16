3ed85e4cee0e59c88a6e34402cfcff2888669c49
Christopher <crobinson@student.neumont.edu>
2017-02-16 12:32:22 -0700

    Clean up mainFrame, remove unnecessary commands

--- src/edu/neumont/csc150/finalProject/MainFrame.java
+++ src/edu/neumont/csc150/finalProject/MainFrame.java
@@ -19,29 +19,28 @@ public class MainFrame extends JFrame {
 	
     private void InitUI() {
     	
-    	InitGame();
-    	
-    	this.setContentPane(C);
+    	setTitle("Fight!");
+    	setDefaultCloseOperation(EXIT_ON_CLOSE);
     	
     	setLayout(null);
     	setResizable(false);
-    	setVisible(true);
     	setBounds((Finals.SCR_WIDTH - Finals.FRAME_WIDTH) / 2, (Finals.SCR_HEIGHT - Finals.FRAME_HEIGHT) / 2, Finals.FRAME_WIDTH + 6, Finals.FRAME_HEIGHT + 51); // 6 & 51 are offsets from the window itself
-    	setTitle("Fight!");
     	setLocationRelativeTo(null);
-    	setDefaultCloseOperation(EXIT_ON_CLOSE);
+    	
+    	setVisible(true);
+    	
+    	InitGame();
+    	
+    	this.setContentPane(C);
     }
     
     
     public void InitGame() {
     	C = new Game();
     	
-    	layout = new GroupLayout(C);
-    	
-    	C.setLayout(layout);
+    	C.setLayout(null);
        	C.setFocusable(true);
         C.setDoubleBuffered(true);
-        C.setPreferredSize(new Dimension(Finals.FRAME_WIDTH, Finals.FRAME_HEIGHT));
         C.setBounds(0, 0, Finals.FRAME_WIDTH, Finals.FRAME_HEIGHT);
     }
     
