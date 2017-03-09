e96c989e7bc1f9727576866451e8250490481270
callmejuan <JReyes@student.neumont.edu>
2017-03-08 19:20:04 -0700

    got the server working with game running and i didn't need pancakes

--- src/edu/neumont/csc150/finalProject/UDPServer.java
+++ src/edu/neumont/csc150/finalProject/UDPServer.java
@@ -1,12 +1,13 @@
 package edu.neumont.csc150.finalProject;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.Timer;
 
-public class UDPServer {
+public class UDPServer implements Runnable{
 	
 	private byte[] receiveData;
 	private byte[] sendData;
@@ -21,37 +22,84 @@ public class UDPServer {
 		initVars();
 		
 		System.out.println("UDP Server Started........");
-		
-		while(true) {			
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-
-			//receives any data
-			serverSocket.receive(receivePacket);
+		Thread serverThread = new Thread(new Runnable() {
 			
-			//gets rid of excess string
-			buttonInput = new String( receivePacket.getData()).substring(0, receivePacket.getLength());
-			
-			System.out.println("RECEIVED: " + buttonInput);			
-			
-			//gets IP address of incoming packets
-			InetAddress IPAddress = receivePacket.getAddress();
-			System.out.println(IPAddress);
-			
-			//gets port from incoming packets
-			int port = receivePacket.getPort();
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				while(true) {			
+					DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-			String id = Integer.toString(assignIDs);
-			sendData = id.getBytes();
+					//receives any data
+					try {
+						serverSocket.receive(receivePacket);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					//gets rid of excess string
+					buttonInput = new String( receivePacket.getData()).substring(0, receivePacket.getLength());
+					
+					System.out.println("RECEIVED: " + buttonInput);			
+					
+					//gets IP address of incoming packets
+					InetAddress IPAddress = receivePacket.getAddress();
+					System.out.println(IPAddress);
+					
+					//gets port from incoming packets
+					int port = receivePacket.getPort();
 
-			if(assignIDs < PlayerID.values().length ) {
-				
-				assignIDs++;
-				
-				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+					String id = Integer.toString(assignIDs);
+					sendData = id.getBytes();
 
-				serverSocket.send(sendPacket);
+					if(assignIDs < PlayerID.values().length ) {
+						
+						assignIDs++;
+						
+						DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+
+						try {
+							serverSocket.send(sendPacket);
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
 			}
-		}
+		});
+		serverThread.start();
+//		while(true) {			
+//			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+//
+//			//receives any data
+//			serverSocket.receive(receivePacket);
+//			
+//			//gets rid of excess string
+//			buttonInput = new String( receivePacket.getData()).substring(0, receivePacket.getLength());
+//			
+//			System.out.println("RECEIVED: " + buttonInput);			
+//			
+//			//gets IP address of incoming packets
+//			InetAddress IPAddress = receivePacket.getAddress();
+//			System.out.println(IPAddress);
+//			
+//			//gets port from incoming packets
+//			int port = receivePacket.getPort();
+//
+//			String id = Integer.toString(assignIDs);
+//			sendData = id.getBytes();
+//
+//			if(assignIDs < PlayerID.values().length ) {
+//				
+//				assignIDs++;
+//				
+//				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+//
+//				serverSocket.send(sendPacket);
+//			}
+//		}
 	}
 	
 	private void initVars() throws SocketException {
@@ -61,6 +109,17 @@ public class UDPServer {
 		sendData = new byte[10];
 		
 		packetHandlers = new PacketHandler[PlayerID.values().length];
+	}
+
+	@Override
+	public void run() {
+		try {
+			UDPServer s = new UDPServer();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
 	}
 	
 }
