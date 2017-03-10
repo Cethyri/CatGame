90d910b7143920475c23a460734778cc8a17af28
Christopher <crobinson@student.neumont.edu>
2017-03-09 23:53:03 -0700

    fix wrong key codes

--- src/edu/neumont/csc150/finalProject/PlayerID.java
+++ src/edu/neumont/csc150/finalProject/PlayerID.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 import java.awt.event.KeyEvent;
 
 public enum PlayerID {
-	FIRST(new Color(102, 0, 0), KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT, KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_PAGE_DOWN, KeyEvent.VK_PAGE_DOWN, 0),
+	FIRST(new Color(102, 0, 0), KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT, KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_PAGE_UP, KeyEvent.VK_PAGE_DOWN, 0),
 	SECOND(new Color(0, 102, 102), KeyEvent.VK_A, KeyEvent.VK_D, KeyEvent.VK_W, KeyEvent.VK_S, KeyEvent.VK_Q, KeyEvent.VK_E, MainFrame.CONTENT_WIDTH / 4),
 	THIRD(new Color(204, 102, 0), KeyEvent.VK_F, KeyEvent.VK_H, KeyEvent.VK_T, KeyEvent.VK_G, KeyEvent.VK_U, KeyEvent.VK_O, MainFrame.CONTENT_WIDTH / 4 * 2),
 	FOURTH(new Color(102, 0, 204), KeyEvent.VK_J, KeyEvent.VK_L, KeyEvent.VK_I, KeyEvent.VK_K, KeyEvent.VK_R, KeyEvent.VK_Y, MainFrame.CONTENT_WIDTH / 4 * 3);
